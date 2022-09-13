8121c551dcb9348d3950c4d07d35f1ba6b37ad9c
Omaranwa <omar.aglan91@yahoo.com>
2022-09-13 07:43:24 +0200

    Variables Test

---
+++ HelloWorld/Main.cpp
@@ -0,0 +1,10 @@
+#define WIN32_LEAN_AND_MEAN
+#define VC_EXTRALEAN
+#include <iostream>
+#include <Windows.h>
+
+int main() 
+{
+	std::cout << "hello World" << std::endl;
+	std::cin.get();
+}
---
+++ Variables/Variables.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+
+int main() 
+{
+	/*
+	
+					Numbers Test 
+	
+	*/
+
+	// int is a signed 4 bytes = 2.2 billion negative or positive numbers
+	int variable = 5;
+
+	// a way to print our values to a console
+	std::cout << variable << std::endl;
+
+	// int is a unsigned 4 bytes = almost 4.1 billion negative only numbers
+	unsigned int variable = 5;
+
+	// a way to print our values to a console
+	std::cout << variable << std::endl;
+
+	// wait for the enter key
+	std::cin.get();
+}
