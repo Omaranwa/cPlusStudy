46e6b7362ea6cc8a7cbe608980b58ff21e52fb08
Omar Aglan <Omar.aglan91@gmail.com>
2022-12-10 15:40:30 +0200

    1

--- Loops/Main.cpp
+++ Loops/Main.cpp
@@ -8,18 +8,24 @@ void log(const char* message)
 
 int main()
 {
+	/// <summary>
+	/// Control Flow
+	/// </summary>
+	/// <returns></returns>
 	for (int i = 0; i < 5; i++)
 	{
 		log("hello World");
 	}
+	/// loop Part Of Training
+	/* 
+			log("________________________________________");
+			int i = 0;
+			while (i < 5)
+			{
+				log("hello World");
+				i++;
+			}
+	*/
 	
-	log("________________________________________");
-
-	int i = 0;
-	while (i < 5)
-	{
-		log("hello World");
-		i++;
-	}
 	std::cin.get();
 }
